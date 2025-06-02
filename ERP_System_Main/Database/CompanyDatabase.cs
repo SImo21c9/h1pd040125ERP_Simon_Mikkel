a8a040f657f8026305b21b368f114c86bc152fda
Superlaban1 <mikkelbach07@gmail.com>
2025-06-02 13:30:14 +0200

    Tried to make Databse.GetAddressById

--- ERP_System_Main/Database/CompanyDatabase.cs
+++ ERP_System_Main/Database/CompanyDatabase.cs
@@ -1,5 +1,6 @@
 namespace ERP_System;
 using TECHCOOL.UI;
+using Microsoft.Data.SqlClient;
 
 // H�ndterer lagring og adgang til virksomhedsdata
 public partial class Database
@@ -35,6 +36,46 @@ public partial class Database
             company.Name = company.CompanyName; // Sørg for at Name også er sat
             companies.Add(company);
         }
+        SqlConnection connection = GetConnection();
+        SqlCommand command = connection.CreateCommand();
+        if (company.CompanyId == 0)
+        {
+            command.CommandText = "INSERT INTO Companies () VALUES (@GF)";
+            command.Parameters.AddWithValue("@CompanyId", nextCompanyId);
+            command.ExecuteNonQuery();
+
+            SqlCommand getScopeIdentityCommand = connection.CreateCommand();
+            getScopeIdentityCommand.CommandText = "SELECT SCOPE_IDENTITY()";
+            SqlDataReader scopeReader = command.ExecuteReader();
+            scopeReader.Read();
+
+            company.CompanyId = (int) scopeReader.GetInt64(0);
+            
+        }
+        else
+        {
+            command.CommandText = "UPDATE Products SET Name = @Name WHERE CompanyId = @CompanyId";
+            command.ExecuteNonQuery();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Company companyAdd = new();
+                companyAdd.CompanyId = reader.GetInt32(0);
+                companyAdd.CompanyName = reader.GetString(50);
+                companyAdd.Name = reader.GetString(60);
+                companyAdd.Street = reader.GetString(100);
+                companyAdd.StreetNumber = reader.GetString(101);
+                companyAdd.City = reader.GetString(102);
+                companyAdd.Address = GetAddressById(reader.GetInt32(4)); //(Address)reader.GetInt32(4);
+                companyAdd.Country = (Country)reader.GetInt32(200);
+                companyAdd.Currency = (Currency)reader.GetInt32(201);
+            }
+        }
+    }
+
+    public Address GetAddressById(int id)
+    {
+
     }
 
     // Opdaterer en eksisterende virksomhed, hvis ID findes
